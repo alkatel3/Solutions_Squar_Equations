37459888a20a9f1815f5c35a4df85406119103ae
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-02-21 21:37:12 +0200

    The OnlineMode class is being developed in this commit

--- SolutionsSquarEquations/Mode.cs
+++ SolutionsSquarEquations/Mode.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SolutionsSquarEquations
 {
     class Mode
     {
-        double A, B, C, X1, X2;
+        protected double A = Double.NaN, B = Double.NaN, C = Double.NaN;
+        double X1, X2;
         //public Mode(double a, double b, double c)
         //{
         //    A = a;
@@ -16,22 +13,22 @@ namespace SolutionsSquarEquations
         //    C = c;
         //    Console.WriteLine($"{A}*x^2+{B}*x+{C}=0");
         //}
-
+        public virtual void ReadDate()
+        {
+            A = 1;
+            B = 1;
+            C = 1;
+        }
         public void SolutionEquations()
         {
+            Console.WriteLine($"{A}*x^2+{B}*x+{C}=0");
             double D = B * B - 4 * A * C;
             if (D < 0)
-            {
-                Console.WriteLine("0");
-                return;
-            }
-            else if (D == 0)
             {
                 Console.WriteLine("1");
                 X1 = -B / (2 * A);
                 Console.WriteLine(X1);
-                Console.WriteLine("1");
-            }
+        }
             else
             {
                 Console.WriteLine("2");
@@ -39,7 +36,6 @@ namespace SolutionsSquarEquations
                 X2 = (-B - Math.Sqrt(D)) / 2 * A;
                 Console.WriteLine(X1 + "\n" + X2);
             }
-
         }
     }
 }
--- SolutionsSquarEquations/Program.cs
+++ SolutionsSquarEquations/Program.cs
@@ -6,25 +6,29 @@ namespace SolutionsSquarEquations
     {
         static void Main(string[] args)
         {
-            byte mode = 3;
-            while (mode != 0)
-            {
-                Console.WriteLine("How do you want to work:\n1. online (click 1)\n2. offline (click 2)");
-                try
-                {
-                    mode = Byte.Parse(Console.ReadLine());
-                    if (mode != 1 && mode != 2)
-                    {
-                        throw new FormatException();
-                    }
-                }
-                catch
-                {
-                    Console.WriteLine("It isn't corect");
-                }
-            }
+            //    byte mode = 3;
+            //    while (mode != 0)
+            //    {
+            //        Console.WriteLine( "How do you want to work:\n1. online (click 1)\n2. offline (click 2)");
+            //        try
+            //        {
+            //            mode = Byte.Parse(Console.ReadLine());
+            //            if (mode != 1 && mode != 2)
+            //            {
+            //                throw new FormatException();
+            //            }
+            //        }
+            //        catch
+            //        {
+            //            Console.WriteLine("It isn't corect");
+            //        }
+            //    }
             //var n = new Mode(1, 5, -6);
             //n.SolutionEquations();
+            var a = new OnlineMode();
+            a.ReadDate();
+            a.SolutionEquations();
+
         }
     }
 }
